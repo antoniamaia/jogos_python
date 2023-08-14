a07c0bff91f5e4d03832e9e704b2a0a228c83fb3
Maria Antonia <mdeazevedomaia@gmail.com>
2023-08-14 18:00:32 -0300

    Avançando no conteúdo

--- forca.py
+++ forca.py
@@ -1,8 +1,32 @@
 def jogar():
     print("**********************************")
-    print("Bem vindo ao jogo de Adivinhação!!")
+    print("***Bem vindo ao jogo da Forca!!***")
     print("**********************************")
     print(end="\n")
+
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    # Enquanto não enforcou e ainda não acertou
+    # Enquanto (true e true)
+    while( not enforcou and not acertou):
+        # Declarando as variáveis para utilizar no laço 
+        chute = input("Qual letra? ")
+        chute = chute.strip() # Tratando a informação enviada pelo jogador, retira todos os espaços
+
+        index = 0
+
+        for letra in palavra_secreta:
+            # Comparação indiferente da letra ser maiúscula ou minúscula
+            if (chute.upper() == letra.upper()):
+               print("Encontrei a letra {} na posição {}".format(chute, index))
+
+            index = index + 1
+
+
+        print("Jogando...")
 
     print("Fim do Jogo")
 
